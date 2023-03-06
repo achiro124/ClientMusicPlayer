d915cee2bd8bef97e081dc1da963fcade452f0cd
максим фадеев <nav.fadeev@mail.ru>
2023-03-06 13:09:11 +0300

    12

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,29 +1,18 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using MusicPlayer.ServiceAudio;
-using System.Windows.Markup;
 using AudioPlayerService;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Reflection.Emit;
 
 namespace MusicPlayer
 {
